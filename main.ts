e85c1a18b63200c03b7a57ac0b53ea484b2a8fc5
shanazsiva1405 <97859615+shanazsiva1405@users.noreply.github.com>
2025-10-28 08:58:15 +0700

    Refactor Firebase data submission and enhance payload
    Updated Firebase URL structure and added detected class and confidence fields.

--- main.ts
+++ main.ts
@@ -32,15 +32,25 @@ Deno.serve(async (req) => {
       const roboflowData = await roboflowRes.json();
 
       // === Kirim hasil ke Firebase ===
-      await fetch(FIREBASE_URL, {
-        method: "POST",
+      // === kirim ke Firebase RTDB ===
+      await fetch(`${FIREBASE_URL}/detections.json`, {
+         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
           imageUrl,
           detection: roboflowData,
-          timestamp: Date.now(),
+          detected_class:
+            roboflowData.predictions && roboflowData.predictions.length > 0
+            ? roboflowData.predictions[0].class
+            : "Tidak terdeteksi",
+          confidence:
+        roboflowData.predictions && roboflowData.predictions.length > 0
+          ? roboflowData.predictions[0].confidence
+          : 0,
+        timestamp: new Date().toISOString(),
         }),
       });
+
 
       return new Response(
         JSON.stringify({ success: true, roboflowData }),
